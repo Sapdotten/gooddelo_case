ad1987d9657a376a755f3c91e04b4381c22ec6f6
Sapdotten <semenova.a.p.04@gmail.com>
2024-07-23 00:23:45 +0300

    add slow api to paths

--- app/routers/task_router.py
+++ app/routers/task_router.py
@@ -1,28 +1,34 @@
-from fastapi import FastAPI
-from fastapi import APIRouter
 
+from fastapi import APIRouter
+from routers.limiter import limiter
 router = APIRouter(
    prefix="/tasks",
    tags=["Записи"],
 )
 
 
+
 @router.post("/")
+@limiter.limit("100/minute")
 async def create_note():
    pass
 
 @router.get("/")
+@limiter.limit("100/minute")
 async def get_all_notes():
    pass
 
 @router.get("/{task_id}")
+@limiter.limit("100/minute")
 async def get_note(task_id):
    pass
 
 @router.put("/{task_id}")
+@limiter.limit("100/minute")
 async def edit_note(task_id):
    pass
 
 @router.delete("/{task_id}")
+@limiter.limit("100/minute")
 async def delete_note(task_id):
    pass
--- app/routers/user_router.py
+++ app/routers/user_router.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-
+from routers.limiter import limiter
 router = APIRouter(
    prefix="/",
    tags=["Работа с пользователями"],
@@ -7,13 +7,16 @@ router = APIRouter(
 
 
 @router.post("/register")
+@limiter.limit("100/minute")
 async def register_user():
    pass
 
 @router.post("/login")
+@limiter.limit("100/minute")
 async def login_user():
    pass
 
 @router.post("/logout")
+@limiter.limit("100/minute")
 async def logout_user():
    pass
