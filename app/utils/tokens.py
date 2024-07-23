a0d60de34aae898a9b754ec63da6bc3ad10c69c4
Sapdotten <semenova.a.p.04@gmail.com>
2024-07-23 14:11:26 +0300

    add token module for working with jwt tokens

---
+++ app/utils/tokens.py
@@ -0,0 +1,44 @@
+import jwt
+from datetime import datetime, timedelta, timezone
+from typing import Union
+
+from app.utils.settings import Settings
+
+
+class TokenManager:
+    __SECRET_KEY = Settings.get_jwt_secret()
+    (
+        ALGORITHM,
+        ACCESS_TOKEN_EXPIRE_MINUTES,
+        REFRESH_TOKEN_EXPIRE_DAYS,
+    ) = Settings.get_jwt_configs()
+
+    @classmethod
+    def create_access_token(cls, data: dict):
+        to_encode = data.copy()
+        expire = datetime.now(timezone.utc) + timedelta(minutes=cls.ACCESS_TOKEN_EXPIRE_MINUTES)
+        to_encode["exp"] = expire
+        encoded_jwt = jwt.encode(to_encode, cls.__SECRET_KEY, algorithm=cls.ALGORITHM)
+        return encoded_jwt
+
+    @classmethod
+    def create_refresh_token(cls, data: dict):
+        to_encode = data.copy()
+        expire = datetime.now(timezone.utc) + timedelta(days=cls.REFRESH_TOKEN_EXPIRE_DAYS)
+        to_encode["exp"] = expire
+        encoded_jwt = jwt.encode(to_encode, cls.__SECRET_KEY, algorithm=cls.ALGORITHM)
+        return encoded_jwt
+
+    @classmethod
+    def decode_token(cls, token: str) -> Union[None, dict]:
+        try:
+            decoded_token = jwt.decode(token, cls.__SECRET_KEY, algorithms=[cls.ALGORITHM])
+            return (
+                decoded_token
+                if decoded_token["exp"] >= datetime.now(timezone.utc).timestamp()
+                else None
+            )
+        except jwt.ExpiredSignatureError:
+            return None
+        except jwt.PyJWTError:
+            return None
