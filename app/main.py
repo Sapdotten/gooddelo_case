73ad1604e3d2a18d76e487cbee7a56a394ddf19a
Sapdotten <semenova.a.p.04@gmail.com>
2024-07-23 19:52:40 +0300

    add include router for token endpoint

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ from slowapi import _rate_limit_exceeded_handler
 from slowapi.errors import RateLimitExceeded
 from contextlib import asynccontextmanager
 from app.routers.limiter import limiter
-from app.routers import task_router, user_router
+from app.routers import task_router, user_router, token_router
 from app.database.init_db import init_models
 import asyncio
 
@@ -18,4 +18,5 @@ app.state.limiter = limiter
 app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 app.include_router(task_router.router)
 app.include_router(user_router.router)
+app.include_router(token_router.router)
 
